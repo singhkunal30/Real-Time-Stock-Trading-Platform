290642b33e3e81fb6f1f5561b1962218517400b2
singhkunal30 <singhkunal30@outlook.com>
2024-09-22 03:00:18 +0530

    Add API gateway and service registry

--- common-trading-service/src/main/java/com/tradingplatform/common/config/RestTemplateConfig.java
+++ common-trading-service/src/main/java/com/tradingplatform/common/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.tradingplatform.common.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,8 +8,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- user-service/src/main/java/com/tradingplatform/user/UserServiceApplication.java
+++ user-service/src/main/java/com/tradingplatform/user/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.tradingplatform.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication()
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.tradingplatform.common", "com.tradingplatform.user"})
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
